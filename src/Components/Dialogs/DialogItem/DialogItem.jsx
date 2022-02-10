68eb825f5a09e898eb58f1e740678c98d5f25bb5
Zhan1313 <zhan.k1313@gmail.com>
2022-02-10 14:12:56 +0600

    added dialogItem avatars

--- src/Components/Dialogs/DialogItem/DialogItem.jsx
+++ src/Components/Dialogs/DialogItem/DialogItem.jsx
@@ -5,6 +5,7 @@ const DialogItem = ({id, name}) => {
     let path = "/dialogs/" + id
     return (
         <div className={s.dialogItem}>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4XSv8zvv1XvIvvz8PFg8hRwUTQJjbis-CJg&usqp=CAU'/>
             <NavLink to={path}>{name}</NavLink>
         </div>
     )
