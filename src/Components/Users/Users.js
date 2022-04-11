ba1b7ba0c795c2217a12cd110608262f112eae47
Zhan1313 <zhan.k1313@gmail.com>
2022-04-11 12:43:28 +0600

    passing all necessary properties and callbacks from state and store into Users functional component

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -3,22 +3,22 @@ import User from "./User";
 import styles from "./User.module.css";
 
 const Users = (props) => {
-    let pagesCount = Math.ceil(this.props.totalUsersCount/this.props.pageSize);
+    let pagesCount = Math.ceil(props.totalUsersCount/props.pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
 
-    let user = this.props.users.map(user => <User key={user.id} userId={user.id} photos={user.photos.small}
+    let user = props.users.map(user => <User key={user.id} userId={user.id} photos={user.photos.small}
                                                   followed={user.followed} name={user.name}
                                                   status={user.status} location={user.location}
-                                                  follow={this.props.follow} unfollow={this.props.unfollow}/>);
+                                                  follow={props.follow} unfollow={props.unfollow}/>);
     return (
         <div>
             <div>
                 {pages.map(pageNumber => {
-                    return <span onClick={(e) => {this.onPageChanged(pageNumber)}}
-                                 className={this.props.currentPage === pageNumber && styles.selectedPage}>
+                    return <span onClick={(e) => {props.onPageChanged(pageNumber)}}
+                                 className={props.currentPage === pageNumber && styles.selectedPage}>
                             {pageNumber}</span>
                 })}
             </div>
--- src/Components/Users/UsersAPIContainer.js
+++ src/Components/Users/UsersAPIContainer.js
@@ -20,7 +20,8 @@ class UsersAPIContainer extends React.Component {
     }
 
     render() {
-        return <Users/>
+        return <Users totalUsersCount={this.props.totalUsersCount} pageSize={this.props.pageSize}
+                      users={this.props.users} follow={this.props.follow} unfollow={this.props.unfollow}/>
     }
 }
 
