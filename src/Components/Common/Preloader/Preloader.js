b0ccab37e1e0716431481ee3b729ba553b0c5f32
Zhan1313 <zhan.k1313@gmail.com>
2022-04-18 11:55:13 +0600

    created Preloader

---
+++ src/Components/Common/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+    return (
+        <div>
+            <img />
+        </div>
+    )
+}
+
+export default Preloader;
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,29 +1,37 @@
 import React from "react";
 import User from "./User";
 import styles from "./User.module.css";
+import Preloader from "../Common/Preloader/Preloader";
 
 const Users = (props) => {
-    let pagesCount = Math.ceil(props.totalUsersCount/props.pageSize);
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
 
     let user = props.users.map(user => <User key={user.id} userId={user.id} photos={user.photos.small}
-                                                  followed={user.followed} name={user.name}
-                                                  status={user.status} location={user.location}
-                                                  follow={props.follow} unfollow={props.unfollow}/>);
+                                             followed={user.followed} name={user.name}
+                                             status={user.status} location={user.location}
+                                             follow={props.follow} unfollow={props.unfollow}/>);
     return (
-        <div>
+        <>
+            <Preloader />
             <div>
-                {pages.map(pageNumber => {
-                    return <span onClick={(e) => {props.onPageChanged(pageNumber)}}
-                                 className={props.currentPage === pageNumber && styles.selectedPage}>
+                <div>
+                    {pages.map(pageNumber => {
+                        return <span onClick={(e) => {
+                            props.onPageChanged(pageNumber)
+                        }}
+                                     className={props.currentPage === pageNumber && styles.selectedPage}>
                             {pageNumber}</span>
-                })}
+                    })}
+                </div>
+                <div>
+                    {user}
+                </div>
             </div>
-            {user}
-        </div>
+        </>
     )
 }
 export default Users;
