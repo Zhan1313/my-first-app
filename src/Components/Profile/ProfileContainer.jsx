a638d25bf2e4230cb5be35d8b7ba8937481a8140
Zhan1313 <zhan.k1313@gmail.com>
2022-04-19 20:37:46 +0600

    added componentDidMount method to ProfileContainer class component and incapsulated ajax/http request for profile(userId)

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Profile from "./Profile";
+import axios from "axios";
 
 class ProfileContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+            .then(response => {
+                this.props.setProfile(response.data);
+            });
+    }
 
     render () {
         return <Profile/>
