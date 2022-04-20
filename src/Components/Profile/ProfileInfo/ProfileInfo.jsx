8ce24304a680733224028e15335e36302f47ebae
Zhan1313 <zhan.k1313@gmail.com>
2022-04-20 13:29:06 +0600

    created 2 images for ProfileInfo component and used them through ternary operation

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,7 @@
 import s from './ProfileInfo.module.css'
 import Preloader from "../../Common/Preloader/Preloader";
+import LookingForJob from '../../../Assets/images/lookingForAJob.png'
+import NotLookingForJob from '../../../Assets/images/notLookingForAJob.jpg'
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -11,9 +13,23 @@ const ProfileInfo = (props) => {
                 src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8elXVuXaPtlBNW136vDKrASrUUZMdbJM9jA&usqp=CAU'/>
         </div>
         <div>
+            <div>
+                {props.profile.fullName}
+            </div>
+            <div>
+                <img src={props.profile.photos.large}/>
+            </div>
+            <div>
+                {props.profile.aboutMe}
+            </div>
+            <div>
+                {props.profile.lookingForAJob ? <img
+                    src={LookingForJob}/> : <img src={NotLookingForJob}/>}
+            </div>
+            <div>
+                {props.profile.lookingForAJobDescription}
+            </div>
 
-            <img src={props.profile.photos.large}/>
-            ava + description
         </div>
     </div>
 }
