56316960abfa2f8ed01879a28584d0b755e9bdea
Zhan1313 <zhan.k1313@gmail.com>
2022-04-21 18:08:03 +0600

    added componentDidMount inside HeaderContainer class component

--- src/Components/Header/HeaderContainer.jsx
+++ src/Components/Header/HeaderContainer.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import {connect} from "react-redux";
 import Header from "./Header";
+import axios from "axios";
 
 class HeaderContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
+            .then(response => {
+                this.props.setUserProfile(response.data);
+            });
+    }
+
     render() {
         return <Header {...this.props}/>
     }
