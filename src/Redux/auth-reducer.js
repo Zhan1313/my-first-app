7fb9c7f512086968b44441e3efe4145c6726ae6b
Zhan1313 <zhan.k1313@gmail.com>
2022-04-21 18:25:14 +0600

    created auth-reducer with setUserAuthDataAC and initial state with email, userId and login, and added authReducer to redux-store into auth branch

---
+++ src/Redux/auth-reducer.js
@@ -0,0 +1,26 @@
+const SET_USER_AUTH_DATA = 'SET_USER_AUTH_DATA';
+
+let initialState = {
+    login: null,
+    email: null,
+    userId: null,
+    isAuth: false
+}
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER_AUTH_DATA:
+            return {
+                ...state,
+                ...action.data,
+                isAuth: true
+            }
+        default:
+            return state;
+    }
+}
+
+export const setUserAuthData = (userId) => ({type: SET_USER_AUTH_DATA, data: {}});
+
+
+export default authReducer;
