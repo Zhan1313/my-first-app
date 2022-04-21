ccea7407530398c26d14759873e55d399942cdf9
Zhan1313 <zhan.k1313@gmail.com>
2022-04-21 07:53:11 +0600

    added withRouter function around ProfileContainer to get access to URL

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -3,9 +3,11 @@ import Profile from "./Profile";
 import axios from "axios";
 import {setUserProfile} from "../../Redux/profile-reducer";
 import {connect} from "react-redux";
+import withRouter from 'react-router-dom'
 
 class ProfileContainer extends React.Component {
     componentDidMount() {
+        debugger;
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
             .then(response => {
                 this.props.setUserProfile(response.data);
@@ -22,4 +24,6 @@ let mapStateToProps = (state) => {
         profile: state.profilePage.profile
     }
 }
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, {setUserProfile})(WithUrlDataContainerComponent);
