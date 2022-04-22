1bc48abe0b9a5f1331cf70a344ce48e7f8cf10ee
Zhan1313 <zhan.k1313@gmail.com>
2022-04-22 12:13:14 +0600

    changed if statement inside ProfileContainer class component into let userId = this.props.match.params.userId || 2

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -7,10 +7,7 @@ import {withRouter} from 'react-router-dom'
 
 class ProfileContainer extends React.Component {
     componentDidMount() {
-        let userId = this.props.match.params.userId;
-        if (!userId) {
-            userId = 2;
-        }
+        let userId = this.props.match.params.userId || 2;
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
             .then(response => {
                 this.props.setUserProfile(response.data);
