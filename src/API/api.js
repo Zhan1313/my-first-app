9f20827371704f039b44cde61fae8df04a43fd84
Zhan1313 <zhan.k1313@gmail.com>
2022-04-23 12:16:11 +0600

    created instance using axios.create in DAL api

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+const baseUrl = `https://social-network.samuraijs.com/api/1.0/`;
+const instance = axios.create({
+    withCredentials: true,
+    headers: {
+        "API-KEY" : "af17f9c2-d67a-4927-b686-20b173009084"
+    }
+})
+
 export const getUsers = (currentPage, pageSize) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
+    return axios.get(`${baseUrl}users?page=${currentPage}&count=${pageSize}`, {
         withCredentials: true
     });
 }
