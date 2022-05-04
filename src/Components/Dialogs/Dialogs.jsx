e8681e90f9f27aee139fb2f54454fdf4b675fa41
Zhan1313 <zhan.k1313@gmail.com>
2022-05-04 23:16:09 +0600

    developed DialogsReduxForm with formData for Dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,9 +2,12 @@ import s from './Dialogs.module.css';
 import React from 'react';
 import DialogItem from "./DialogItem/DialogItem";
 import DialogMessage from "./DialogMessage/DialogMessage";
+import {Field, reduxForm} from "redux-form";
 
-const Dialogs = ({dialogItems, dialogMessages, onNewDialogMessageChange,
-                     onSendNewDialogsMessage, newDialogMessageText}) => {
+const Dialogs = ({
+                     dialogItems, dialogMessages, onNewDialogMessageChange,
+                     onSendNewDialogsMessage, newDialogMessageText
+                 }) => {
     let dialogsItems = dialogItems.map(el => <DialogItem name={el.name} id={el.id}/>)
     let dialogsMessages = dialogMessages.map(el => <DialogMessage message={el.message}/>)
 
@@ -15,25 +18,32 @@ const Dialogs = ({dialogItems, dialogMessages, onNewDialogMessageChange,
         let messageText = e.target.value;
         onNewDialogMessageChange(messageText);
     }
+    let onSubmit = (formData) => {
+        console.log(formData);
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 {dialogsItems}
             </div>
             <div className={s.dialogsMessages}>
-                <div>
-                    {dialogsMessages}
-                </div>
-                <div>
-                    <textarea value={newDialogMessageText} onChange={onNewDialogMessageChangeClick}/>
-                </div>
-                <div>
-                    <button onClick={onSendNewDialogsMessageClick}>Send</button>
-                </div>
-
-
+                {dialogsMessages}
             </div>
+            <DialogsReduxForm onSubmit={onSubmit}/>
         </div>
     )
 }
+const DialogsForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'type message here...'} component={'textarea'} name={'messageText'}/>
+            </div>
+            <div>
+                <button>Send</button>
+            </div>
+        </form>
+    )
+}
+const DialogsReduxForm = reduxForm({form: 'message'})(DialogsForm);
 export default Dialogs;
