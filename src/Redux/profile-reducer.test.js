e84a1ed942d3383ed31095ab49c6c4b6865c800f
Zhan1313 <zhan.k1313@gmail.com>
2022-06-08 18:23:26 +0600

    successfully tested profileReducers AddPost case

--- src/Redux/profile-reducer.test.js
+++ src/Redux/profile-reducer.test.js
@@ -1,19 +1,25 @@
 import profileReducer, {addPostActionCreator} from "./profile-reducer";
-import {render, screen} from "@testing-library/react";
-import App from "../App";
 
+let state = {
+    postsData: [
+        {id: 1, message: 'Hello, how are you, mate?', likesCount: '345'},
+        {id: 2, message: 'I am ok, how r u urself?', likesCount: '334'}
+    ]
+};
 test('length of postsData should be incremented', () => {
     // 1. test data
     let action = addPostActionCreator('It-Kamasutra');
-    let state = {
-        postsData: [
-            {id: 1, message: 'Hello, how are you, mate?', likesCount: '345'},
-            {id: 2, message: 'I am ok, how r u urself?', likesCount: '334'}
-        ]
-    };
     // 2. action
     let newState = profileReducer(state, action);
     // 3. expectation
     expect(newState.postsData.length).toBe(3);
+});
+test('message of new post should be correct', () => {
+    // 1. test data
+    let action = addPostActionCreator('It-Kamasutra');
+    // 2. action
+    let newState = profileReducer(state, action);
+    // 3. expectation
+    expect(newState.postsData[2].message).toBe('It-Kamasutra');
 });
 
