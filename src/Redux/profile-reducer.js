f973d7423c2593c3eb4b96ad0f2a26d1606959d6
Zhan1313 <zhan.k1313@gmail.com>
2022-06-08 19:53:45 +0600

    implemented TDD approach for profileReducer testing by initializing non existing deletePost action

--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -1,6 +1,7 @@
 import {profileAPI} from "../API/api";
 
 const ADD_POST = 'ADD_POST';
+const DELETE_POST = 'DELETE_POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
@@ -28,6 +29,11 @@ const profileReducer = (state = initialState, action) => {
                 postsData: [...state.postsData, newPost],
                 newPostText: ''
             };
+        case DELETE_POST:
+            return {
+                ...state,
+                postsData: state.postsData.filter(post => post.id !== action.postId)
+            };
         case UPDATE_NEW_POST_TEXT:
             return {
                 ...state,
@@ -49,6 +55,7 @@ const profileReducer = (state = initialState, action) => {
     }
 }
 export const addPostActionCreator = (postText) => ({type: ADD_POST, postText});
+export const deletePost = (postId) => ({type: DELETE_POST, postId});
 export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text});
 const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 const setStatus = (status) => ({type: SET_STATUS, status});
--- src/Redux/profile-reducer.test.js
+++ src/Redux/profile-reducer.test.js
@@ -1,4 +1,4 @@
-import profileReducer, {addPostActionCreator} from "./profile-reducer";
+import profileReducer, {addPostActionCreator, deletePost} from "./profile-reducer";
 
 let state = {
     postsData: [
@@ -22,4 +22,11 @@ test('message of new post should be correct', () => {
     // 3. expectation
     expect(newState.postsData[2].message).toBe('It-Kamasutra');
 });
-
+test('length of postsData should be decreased', () => {
+    // 1. test data
+    let action = deletePost(1);
+    // 2. action
+    let newState = profileReducer(state, action);
+    // 3. expectation
+    expect(newState.postsData.length).toBe(1);
+});
